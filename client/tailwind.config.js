a207f98b8a17066fff83b49c64b8084d0a1973d7
devhasibulislam <devhasibulislam@gmail.com>
2024-06-16 19:56:12 +0600

    BREAKING CHANGE: replace styles
    Make new styles and integrate with the replace with older ones.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,15 +4,46 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      borderRadius: {
+        primary: "24px",
+        secondary: "9999px",
+        neutral: "5px",
+      },
+      colors: {
+        primary: "#5A639C",
+        secondary: "#219C90",
+        neutral: "#102C57",
+      },
+      fontSize: {
+        primary: "16px",
+        secondary: "24px",
+        neutral: "12px",
+      },
+      fontFamily: {
+        primary: "Poppins",
+        secondary: "Ubuntu",
+        neutral: "Inter",
+      },
+      boxShadow: {
+        primary: "0px 0px 24px rgba(90, 99, 156, 0.4)",
+        secondary: "0px 0px 24px rgba(33, 156, 144, 0.4)",
+        neutral: "0px 0px 24px rgba(16, 44, 87, 0.4)",
+      },
+      padding: {
+        primary: "24px",
+        secondary: "16px",
+        neutral: "8px",
+      },
+      margin: {
+        primary: "24px",
+        secondary: "16px",
+        neutral: "8px",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar-hide")],
 };
