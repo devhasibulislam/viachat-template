e3bb8596312f4c49d864034ad6c16f2bb3ba19be
devhasibulislam <devhasibulislam@gmail.com>
2024-06-18 13:21:19 +0600

    feat: integrate disability
    Add a disability to the button that hold the functionality till the progress is done.

--- client/src/components/auth/Button.jsx
+++ client/src/components/auth/Button.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Button = ({ className, children, ...props }) => {
   return (
-    <button {...props} className={`px-4 py-2 bg-neutral rounded-neutral ${className}`}>
+    <button
+      {...props}
+      className={`px-4 py-2 bg-neutral rounded-neutral disabled:bg-neutral/30 ${className}`}
+    >
       {children}
     </button>
   );
